c08d8e620d54b56ec00585ceca48d4b46800ea92
Manuel Covas <manuel.p.covas@outlook.com>
2019-12-19 20:22:55 +0000

    Finished. Added plenty.

--- commentme1st.js
+++ commentme1st.js
@@ -22,12 +22,27 @@ const oauthTokenRequest = {
   method: "POST",
   path: "/token"
 }
+const commentRequest = {
+  headers: { "Content-Type": "application/json" },
+  host: "www.googleapis.com",
+  method: "POST",
+  path: "/youtube/v3/commentThreads?part=snippet"
+}
 
 
 const API_SECRETS_FILENAME = "apiSecrets.json";
 const ACCOUNTS_FILENAME = "storedAccounts.json";
-var SIGN_IN_URL = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=http://localhost&scope=https://www.googleapis.com/auth/youtube.force-ssl&access_type=offline&prompt=consent";
+const COMMENTS_FILENAME = "storedComments.json";
+var SIGN_IN_URL = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=https://www.googleapis.com/auth/youtube.force-ssl&access_type=offline&prompt=consent";
+var BASE_CHANNELS_URL = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails";
+var BASE_PLAYLIST_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=5";
 
+var channelId = "";
+var uploadsPlaylistId = "";
+var listDelay = -1;
+var existingVideos = [];
+
+var sotredComments = ["Gotta go fast!"];
 var storedAccounts = [];
 var apiSecrets = {
   clientId: "",
@@ -38,21 +53,28 @@ var apiSecrets = {
 
 loadSecrets();
 loadAccounts();
+loadComments();
 SIGN_IN_URL = SIGN_IN_URL.concat("&client_id=", apiSecrets.clientId);
+BASE_CHANNELS_URL = BASE_CHANNELS_URL.concat("&key=", apiSecrets.apiKey);
+BASE_PLAYLIST_URL = BASE_PLAYLIST_URL.concat("&key=", apiSecrets.apiKey);
 rl.prompt();
-
 
 
 function handleCommand(command) {
   switch (command)
   {
-    case "list":
+    case "accounts":
       listLoadedAccounts();
       rl.prompt();
     break;
 
+    case "comments":
+      listComments();
+      rl.prompt();
+    break;
+
     case "add":
-      console.log("#  Adding new account to storage:");
+      console.log("#\n#  Adding new account to storage:");
       console.log("#");
       console.log("#  Sign in: "+SIGN_IN_URL);
       console.log("#");
@@ -60,11 +82,19 @@ function handleCommand(command) {
     break;
 
     case "refresh":
-      rl.question("#  Account number > ", refreshAccount);
+      rl.question("#\n#  Account number > ", refreshAccount);
       break;
 
     case "start":
-      rl.question("#  Target Channel ID > ", watch);
+      rl.question("#  How long to wait between video lookups (ms) > ", (answer) => {
+        listDelay = parseInt(answer.trim());
+        rl.question("#  Target Channel ID > ", (answer) => {
+          channelId = answer.trim();
+          getUploadsPlaylist(() => {
+            getVideos(uploadsPlaylistId, startWatching, true);
+          });
+        });
+      });
       break;
 
     case "exit":
@@ -82,15 +112,17 @@ function exchangeOAuthCode(oAuthCode) {
   var responseData = [];
   var req = https.request(oauthTokenRequest, (res) => {
     console.log("#  Got: "+res.statusCode+" "+res.statusMessage);
-    if (res.statusCode != 200) {
-      console.log("#  Failed to exchange oAuth code for tokens!\n#");
-      rl.prompt();
-      return;
-    }
+
     res.on("data", (d) => {
       responseData.push(d);
     });
     res.on("end", () => {
+      if (res.statusCode != 200) {
+        console.log("#  Failed to exchange oAuth code for tokens!\n#");
+        console.log("#  "+responseData);
+        rl.prompt();
+        return;
+      }
       nameAccount(responseData);
     });
   });
@@ -103,7 +135,7 @@ function exchangeOAuthCode(oAuthCode) {
     "client_id": apiSecrets.clientId,
     "client_secret": apiSecrets.clientSecret,
     "code": oAuthCode.trim(),
-    "redirect_uri": "http://localhost",
+    "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
     "grant_type": "authorization_code"
   });
 
@@ -167,10 +199,171 @@ function storeNewToken(index, newAccessToken) {
 }
 
 
-function watch(channelId) {
+function getUploadsPlaylist(callback) {
+  var getUrl = BASE_CHANNELS_URL.concat("&id=", channelId);
   
+  var responseData = [];
+  var req = https.get(getUrl, (res) => {
+    console.log("#\n#  Getting channel uploads playlist: "+res.statusCode+" "+res.statusMessage);
+    if (res.statusCode != 200) {
+      console.log("#\n#  Failed to get channel uploads!");
+      console.log("#    "+res.statusCode+" "+res.statusMessage+"\n#");
+      rl.prompt();
+      return;
+    }
+    
+    res.on("data", (d) => {
+      responseData.push(d);
+    });
+
+    res.on("end", () => {
+      if (res.statusCode != 200) {
+        console.log("#\n#  Failed to get channel uploads!");
+        console.log("#    "+res.statusCode+" "+res.statusMessage+"\n#");
+      }
+
+      var response = JSON.parse(responseData);
+      uploadsPlaylistId = response.items[0].contentDetails.relatedPlaylists.uploads;
+
+      console.log("#  Got "+channelId+"'s uploads playlist.\n#");
+      callback();
+    });
+  });
+  req.on("error", (e) => {
+    console.log("#  Failed to search for videos!");
+    console.log("#  "+e+"\n#");
+    rl.prompt();
+  });
 }
 
+
+function startWatching(playlistId, fetchedVideos) {
+  existingVideos = fetchedVideos;
+  console.log("#  Now waiting for new video...");
+
+  setTimeout(() => {
+    console.log("#  Fetching...");
+    getVideos(playlistId, watching, false);
+  }, listDelay);
+}
+
+function watching(channelId, fetchedVideos) {
+  var newVideos = [];
+
+  fetchedVideos.forEach(element => {
+    if (!existingVideos.includes(element)) {
+      newVideos.push(element);
+      existingVideos.push(element);
+    }
+  });
+
+  if (newVideos.length == 0)
+  {
+    console.log("#  Waiting "+listDelay+"ms...");
+    setTimeout(() => {
+      console.log("#  Fetching...");
+      getVideos(channelId, watching, false);
+    }, listDelay);
+  }else{
+    console.log("#  Found new video! ("+newVideos.length+")\n#");
+    newVideos.forEach((videoId) => {
+      console.log("#  Commenting on: https://www.youtube.com/watch?v="+videoId);
+      storedAccounts.forEach((account, index) => {
+        comment(videoId, account, index);
+      });
+      console.log("#");
+    });
+    
+    console.log("#  Done commenting.\n#")
+    getVideos(channelId, startWatching, true);
+  }
+}
+
+
+function comment(videoId, account, commentIndex) {
+  commentRequest.headers.Authorization = "Bearer "+account.access_token.token;
+
+  var responseData = [];
+  var comment = sotredComments[commentIndex];
+  var req = https.request(commentRequest, (res) => {
+    console.log("#  "+account.name+" "+res.statusCode+" "+res.statusMessage);
+
+    res.on("data", (d) => {
+      responseData.push(d);
+    });
+    res.on("end", () => {
+      if (res.statusCode != 200) {
+        console.log("#  Failed to comment with "+account.name+"!");
+        console.log("#  "+responseData);
+      }else{
+        console.log("#  "+comment);
+      }
+    });
+  });
+  req.on('error', (e) => {
+    console.log(e);
+    console.log();
+  });
+
+  var payload = JSON.stringify({
+    "snippet": {
+      "channelId": channelId,
+      "videoId": videoId,
+      "topLevelComment": {
+        "snippet": {
+          "textOriginal": comment
+        }
+      }
+    }});
+
+  req.write(payload);
+  req.end();
+}
+
+
+function getVideos(playlistId, callback, logAll) {
+  var playlistGetUrl = BASE_PLAYLIST_URL.concat("&playlistId="+playlistId);
+
+  var videoIds = [];
+  var videoDates = [];
+  var responseData = [];
+  var req = https.get(playlistGetUrl, (res) => {
+    if (logAll) { console.log("#\n#  Listed: "+res.statusCode+" "+res.statusMessage); }
+    if (res.statusCode != 200) {
+      console.log("#\n#  Failed to list videos!");
+      console.log("#    "+res.statusCode+" "+res.statusMessage+"\n#");
+      rl.prompt();
+      return;
+    }
+    res.on("data", (d) => {
+      responseData.push(d);
+    });
+    res.on("end", () => {
+      var response = JSON.parse(responseData);
+      response.items.forEach(element => {
+        videoIds.push(element.contentDetails.videoId);
+        videoDates.push(element.contentDetails.videoPublishedAt);
+      });
+      
+      if (logAll) {
+        console.log("#  Got "+videoIds.length+" latest uploads:\n#");
+        videoDates.forEach((element) => {
+          console.log("#    "+element);
+        });
+        console.log("#");
+      }
+      
+      callback(playlistId, videoIds);
+    });
+  });
+  req.on("error", (e) => {
+    console.log("#  Failed to search for videos!");
+    console.log("#  "+e+"\n#");
+    rl.prompt();
+  });
+  
+  req.end();
+}
 
 function loadSecrets() {
   if (!fs.existsSync(API_SECRETS_FILENAME)) {
@@ -186,6 +379,7 @@ function loadSecrets() {
   }
 }
 
+
 function loadAccounts() {
   if (!fs.existsSync(ACCOUNTS_FILENAME))
   {
@@ -199,18 +393,47 @@ function loadAccounts() {
   }
 }
 
-
 function listLoadedAccounts() {
   if (storedAccounts.length == 0) {
-    console.log("#  No accounts are loaded.");
+    console.log("#\n#  No accounts are loaded.");
   }else{
-    console.log("#  "+storedAccounts.length+" accounts are loaded:");
-    storedAccounts.forEach(element => {
+    console.log("#\n#  "+storedAccounts.length+" accounts are loaded:");
+    storedAccounts.forEach((element, index) => {
       var timeRemaining = (element.access_token.expires_at - Date.now()) / 1000;
       var expired = timeRemaining <= 0;
       
-      console.log("#  > #"+(storedAccounts.indexOf(element)+1)+" "+((!expired) ? "\x1b[32m" : "\x1b[31m")+element.name+"\x1b[0m - last refrshed at "+(new Date(element.access_token.aquired_at).toISOString())+", "+timeRemaining+" seconds remaining.");
+      console.log("#  > #"+(index+1)+" "+((!expired) ? "\x1b[32m" : "\x1b[31m")+element.name+"\x1b[0m - last refrshed at "+(new Date(element.access_token.aquired_at).toISOString())+", "+timeRemaining+" seconds remaining.");
     });
     console.log("#");
   }
+}
+
+
+function loadComments() {
+  if (!fs.existsSync(COMMENTS_FILENAME))
+  {
+    console.log("Initializing inexisting comments storage.");
+    fs.writeFileSync(COMMENTS_FILENAME, JSON.stringify(sotredComments));
+    console.log("Loaded 1 comments.");
+  }else{
+    console.log("Loading stored comments...");
+    sotredComments = JSON.parse(fs.readFileSync(COMMENTS_FILENAME));
+    console.log("Loaded "+sotredComments.length+" comments:");
+  }
+}
+
+function listComments() {
+  console.log("#\n#  Loaded comments:\n#");
+  sotredComments.forEach((element, index) => {
+    console.log("#   #"+(index+1)+" - "+element);
+  });
+  console.log("#");
+}
+
+
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
